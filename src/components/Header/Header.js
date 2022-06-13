08e1c4a9a587b55de7090d9a4cf1e263249b0dd6
pedrocatins29 <pedrocatins29@hotmail.com>
2022-06-13 20:16:47 -0300

    - Adicionando assets do projeto - Configurando absolute imports - Header da pagina Home feito

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="container">
+      <img alt="Marvel Logo" src="assets/logo.svg" />
+      <h1>EXPLORE O UNIVERSO</h1>
+      <p>Mergulhe no domínio deslumbrante de todos os personagens clássicos que você ama - e aqueles que você descobrirá em breve</p>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
+import Header from "components/Header/Header";
 
 function Home() {
   return (
     <div>
-        <p>
-         teste
-        </p>
+      <Header/>
     </div>
   );
 }
