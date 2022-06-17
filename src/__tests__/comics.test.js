f1e2bbe30f9cc28a47784e1c25666f170df089b8
pedrocatins29 <pedrocatins29@hotmail.com>
2022-06-17 02:37:05 -0300

    feat: teste basico de comics

---
+++ src/__tests__/comics.test.js
@@ -0,0 +1,12 @@
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import LatestComics from "components/LatestComics/LatestComics";
+import comics from "__tests__/mocks/comics.json";
+
+test("deve renderizar o titulo e imagem do quadrinho", () => {
+  render(<LatestComics comics={comics} />);
+
+  expect(screen.getByText("Hulk: The End Premiere (Hardcover)")).toBeDefined();
+  expect(screen.queryByAltText("Capa do quadrinho")).toBeVisible();
+});
